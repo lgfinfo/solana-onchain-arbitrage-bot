039689c157f0ded0cd582fe3d7c5121889fd997d
cetipoo <cetipo@hotmail.com>
2025-06-26 20:03:11 -0400

    Remove TODO comment for Vertigo program ID and set the actual program ID in `constants.rs`

--- src/dex/vertigo/constants.rs
+++ src/dex/vertigo/constants.rs
@@ -1,7 +1,6 @@
 use solana_program::pubkey::Pubkey;
 use std::str::FromStr;
 
-// TODO: Replace with actual Vertigo program ID once available
 pub fn vertigo_program_id() -> Pubkey {
     Pubkey::from_str("vrTGoBuy5rYSxAfV3jaRJWHH6nN9WK4NRExGxsk1bCJ").unwrap()
 }
